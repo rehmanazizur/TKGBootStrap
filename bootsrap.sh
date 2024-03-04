0a9a205ce0f92c64c8b603e0a080538d382d5b81
rehmanazizur <30284849+rehmanazizur@users.noreply.github.com>
2024-03-04 17:24:34 +0400

    Update bootsrap.sh

--- bootsrap.sh
+++ bootsrap.sh
@@ -27,7 +27,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 echo "Installed docker!!"
 
-sudo groupadd docker
+sudo groupadd docker || true
 sudo usermod -aG docker $USER
 newgrp docker
 
